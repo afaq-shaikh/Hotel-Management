044671c105eb1cf402645cf4a77bf2c0076c4395
Afaq Shaikh <135786931+afaq-shaikh@users.noreply.github.com>
2023-08-06 16:10:08 +0530

    Update HotelSystem.cpp

--- HotelSystem.cpp
+++ HotelSystem.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <iomanip> // For std::fixed and std::setprecision
+#include <iomanip> 
 #include <vector>
 
 class Hotel {
 private:
-    struct Room {
+    struct Room 
+    {
         std::string type;
         double price;
         bool available;
@@ -22,19 +23,26 @@ private:
     double totalRevenue = 0.0;
 
 public:
-    void addRoom(int roomNumber, const std::string& roomType, double roomPrice, const std::string& amenities) {
-        if (rooms.find(roomNumber) == rooms.end()) {
+    void addRoom(int roomNumber, const std::string& roomType, double roomPrice, const std::string& amenities)
+    {
+        if (rooms.find(roomNumber) == rooms.end()) 
+        {
             Room newRoom{roomType, roomPrice, true, false, amenities};
             rooms[roomNumber] = newRoom;
-        } else {
+        }
+        else 
+        {
             std::cout << "Room number already exists." << std::endl;
         }
     }
 
-    void bookRoom(int roomNumber, const std::string& customerName, const std::string& customerContact, const std::string& checkInDate, const std::string& checkOutDate) {
+    void bookRoom(int roomNumber, const std::string& customerName, const std::string& customerContact, const std::string& checkInDate, const std::string& checkOutDate) 
+    {
         auto it = rooms.find(roomNumber);
-        if (it != rooms.end()) {
-            if (it->second.available) {
+        if (it != rooms.end()) 
+        {
+            if (it->second.available) 
+            {
                 it->second.available = false;
                 it->second.checkedIn = true;
                 it->second.customerName = customerName;
@@ -43,56 +51,80 @@ public:
                 it->second.checkOutDate = checkOutDate;
                 totalRevenue += it->second.price;
                 std::cout << "Room " << roomNumber << " has been booked for " << customerName << "." << std::endl;
-            } else {
+            } 
+            else
+            {
                 std::cout << "Room " << roomNumber << " is not available." << std::endl;
             }
-        } else {
+        } 
+        else 
+        {
             std::cout << "Room number does not exist." << std::endl;
         }
     }
 
-    void cancelBooking(int roomNumber) {
+    void cancelBooking(int roomNumber) 
+    {
         auto it = rooms.find(roomNumber);
-        if (it != rooms.end()) {
-            if (!it->second.available) {
+        if (it != rooms.end()) 
+        {
+            if (!it->second.available) 
+            {
                 it->second.available = true;
                 it->second.checkedIn = false;
                 totalRevenue -= it->second.price;
                 std::cout << "Booking for Room " << roomNumber << " has been canceled." << std::endl;
-            } else {
+            } 
+            else
+            {
                 std::cout << "No booking found for Room " << roomNumber << "." << std::endl;
             }
-        } else {
+        } 
+        else
+        {
             std::cout << "Room number does not exist." << std::endl;
         }
     }
 
-    void checkAvailability(int roomNumber) {
+    void checkAvailability(int roomNumber)
+    {
         auto it = rooms.find(roomNumber);
-        if (it != rooms.end()) {
-            if (it->second.available) {
+        if (it != rooms.end()) 
+        {
+            if (it->second.available) 
+            {
                 std::cout << "Room " << roomNumber << " is available." << std::endl;
-            } else {
+            } 
+            else 
+            {
                 std::cout << "Room " << roomNumber << " is not available." << std::endl;
             }
-        } else {
+        } 
+        else 
+        {
             std::cout << "Room number does not exist." << std::endl;
         }
     }
 
-    void displayAvailableRooms() {
+    void displayAvailableRooms()
+    {
         std::cout << "Available Rooms:" << std::endl;
-        for (const auto& [roomNumber, room] : rooms) {
-            if (room.available) {
+        for (const auto& [roomNumber, room] : rooms)
+        {
+            if (room.available)
+            {
                 std::cout << "Room " << roomNumber << ": Type - " << room.type << ", Price - $" << std::fixed << std::setprecision(2) << room.price << std::endl;
             }
         }
     }
 
-    void displayBookedRooms() {
+    void displayBookedRooms()
+    {
         std::cout << "Booked Rooms:" << std::endl;
-        for (const auto& [roomNumber, room] : rooms) {
-            if (!room.available) {
+        for (const auto& [roomNumber, room] : rooms)
+        {
+            if (!room.available) 
+            {
                 std::cout << "Room " << roomNumber << ": Type - " << room.type << ", Price - $" << std::fixed << std::setprecision(2) << room.price;
                 std::cout << ", Checked-in: " << (room.checkedIn ? "Yes" : "No");
                 std::cout << ", Customer Name: " << room.customerName << ", Customer Contact: " << room.customerContact;
@@ -101,31 +133,39 @@ public:
         }
     }
 
-    void displayAllRooms() {
+    void displayAllRooms() 
+    {
         std::cout << "All Rooms:" << std::endl;
-        for (const auto& [roomNumber, room] : rooms) {
+        for (const auto& [roomNumber, room] : rooms) 
+        {
             std::cout << "Room " << roomNumber << ": Type - " << room.type << ", Price - $" << std::fixed << std::setprecision(2) << room.price;
             std::cout << ", Available: " << (room.available ? "Yes" : "No") << ", Checked-in: " << (room.checkedIn ? "Yes" : "No");
             std::cout << ", Amenities: " << room.amenities << std::endl;
         }
     }
 
-    void displayTotalRevenue() {
+    void displayTotalRevenue() 
+    {
         std::cout << "Total Revenue: $" << std::fixed << std::setprecision(2) << totalRevenue << std::endl;
     }
 
-    void modifyRoomPrice(int roomNumber, double newPrice) {
+    void modifyRoomPrice(int roomNumber, double newPrice) 
+    {
         auto it = rooms.find(roomNumber);
-        if (it != rooms.end()) {
+        if (it != rooms.end())
+        {
             it->second.price = newPrice;
             std::cout << "Room " << roomNumber << " price has been updated to $" << std::fixed << std::setprecision(2) << newPrice << std::endl;
-        } else {
+        } 
+        else
+        {
             std::cout << "Room number does not exist." << std::endl;
         }
     }
 
-    // Function to display the menu and get user choice
-    int displayMenu() {
+   
+    int displayMenu() 
+    {
         int choice;
         std::cout << "\n------------ Hotel Management System Menu ------------\n";
         std::cout << "1. Display Available Rooms\n";
@@ -142,8 +182,8 @@ public:
         return choice;
     }
 
-    // Function to handle user's choice
-    void handleChoice() {
+    void handleChoice()
+    {
         int choice;
         while (true) {
             choice = displayMenu();
@@ -179,8 +219,8 @@ public:
         }
     }
 
-    // Function to book a room interactively by taking user input
-    void bookRoomInteractive() {
+    void bookRoomInteractive()    
+    {
         int roomNumber;
         std::string customerName, customerContact, checkInDate, checkOutDate;
 
@@ -188,8 +228,10 @@ public:
         std::cin >> roomNumber;
 
         auto it = rooms.find(roomNumber);
-        if (it != rooms.end()) {
-            if (it->second.available) {
+        if (it != rooms.end())
+        {
+            if (it->second.available) 
+            {
                 std::cout << "Enter customer name: ";
                 std::cin.ignore();
                 std::getline(std::cin, customerName);
@@ -204,24 +246,28 @@ public:
                 std::getline(std::cin, checkOutDate);
 
                 bookRoom(roomNumber, customerName, customerContact, checkInDate, checkOutDate);
-            } else {
+            } 
+            else
+            {
                 std::cout << "Room " << roomNumber << " is not available for booking." << std::endl;
             }
-        } else {
+        }   
+        else
+        {
             std::cout << "Room number does not exist." << std::endl;
         }
     }
 
-    // Function to cancel a booking interactively by taking user input
-    void cancelBookingInteractive() {
+    void cancelBookingInteractive()    
+    {
         int roomNumber;
         std::cout << "Enter the room number to cancel the booking: ";
         std::cin >> roomNumber;
         cancelBooking(roomNumber);
     }
 
-    // Function to modify room price interactively by taking user input
-    void modifyRoomPriceInteractive() {
+    void modifyRoomPriceInteractive() 
+    {
         int roomNumber;
         double newPrice;
         std::cout << "Enter the room number to modify the price: ";
@@ -232,13 +278,13 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Hotel hotel;
     hotel.addRoom(101, "Standard", 100, "Wi-Fi, TV");
     hotel.addRoom(102, "Deluxe", 150, "Wi-Fi, TV, Minibar");
     hotel.addRoom(103, "Suite", 200, "Wi-Fi, TV, Minibar, Jacuzzi");
 
-    hotel.handleChoice(); // Display the menu and interact with the user
-
+    hotel.handleChoice(); 
     return 0;
 }
